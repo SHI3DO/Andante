fd0ba1d77882027a80dae0e716107edef0dac133
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-22 18:03:13 +0900

    path

--- Andante.py
+++ Andante.py
@@ -1,40 +1,38 @@
+import random
+
 import pygame
 from pygame.locals import *
 import sys
 import os
-
-
-def resource_path(relative_path):
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception as e:
-        print(e)
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
+from utils.bg import get_bg
+from game import screen_background
+from utils import resourcepath
 
 pygame.init()
-icon = pygame.image.load(resource_path("src/icon.png"))
+icon = pygame.image.load(resourcepath.resource_path("src/icon.png"))
 pygame.display.set_icon(icon)
-screensize = pygame.display.Info()
 flags = DOUBLEBUF  # | FULLSCREEN
-# screen = pygame.display.set_mode((screensize.current_w, screensize.current_h))
 screen = pygame.display.set_mode((1280, 720), flags)
+screenx, screeny = screen.get_size()
 pygame.display.set_caption("Andante")
 
 clock = pygame.time.Clock()
 
+bgarray = get_bg.get("src/bg", pygame, screenx, screeny)
+bgnum = random.randrange(0, len(bgarray))
+print(bgnum)
+
 mainLoop = True
 while mainLoop:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            mainLoop = False
-    screen.fill((0, 0, 0))
     events = pygame.event.get()
-    dt = clock.tick(120)
 
+    if events:
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                mainLoop = False
+
+    dt = clock.tick(120)
+    screen_background.draw(screen, bgarray[bgnum])
     pygame.display.flip()
 
 pygame.quit()
