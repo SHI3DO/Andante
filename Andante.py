640fbc216acdcc00709c42474a407f7c78f4dee3
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-25 19:30:15 +0900

    fff

--- Andante.py
+++ Andante.py
@@ -8,6 +8,7 @@ from utils import resourcepath
 from utils import essfolder
 from game import screen_upbar
 from game import maps_container
+from utils.game import mapparser
 
 # //////////////////////////////////////////////////////////////////////////////
 essfolder.make()
@@ -15,11 +16,21 @@ essfolder.make()
 pygame.init()
 icon = pygame.image.load(resourcepath.resource_path("src/icon.png"))
 pygame.display.set_icon(icon)
-flags = DOUBLEBUF  # | FULLSCREEN
+flags = DOUBLEBUF  #| FULLSCREEN
 screen = pygame.display.set_mode((1280, 720), flags)
 screenx, screeny = screen.get_size()
 pygame.display.set_caption("Andante")
 clock = pygame.time.Clock()
+# //////////////////////////////////////////////////////////////////////////////
+LightFont15 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Light.ttf"), int(screeny / 15))
+LightFont20 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Light.ttf"), int(screeny / 20))
+LightFont25 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Light.ttf"), int(screeny / 25))
+LightFont30 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Light.ttf"), int(screeny / 30))
+LightFont35 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Light.ttf"), int(screeny / 35))
+LightFont40 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Light.ttf"), int(screeny / 40))
+RegularFont30 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Regular.ttf"), int(screeny / 30))
+BoldFont30 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Bold.ttf"), int(screeny / 30))
+BoldFont25 = pygame.font.Font(resourcepath.resource_path("src/fonts/Pretendard-Bold.ttf"), int(screeny / 25))
 # //////////////////////////////////////////////////////////////////////////////
 bgarray = get_bg.get("src/bg", pygame, screenx, screeny)
 bgnum = random.randrange(0, len(bgarray))
@@ -46,9 +57,9 @@ while mainLoop:
 
     screen_background.draw(screen, bgarray[bgnum])
     screen_upbar.draw(screen, pygame, screenx, screeny)
-    maps_container.draw(screen, pygame, screenx, screeny, maplist)
+    maps_container.draw(screen, pygame, screenx, screeny, maplist, BoldFont25, LightFont40)
     pygame.display.flip()
-    print(clock.get_fps())
+    #print(clock.get_fps())
 # //////////////////////////////////////////////////////////////////////////////
 pygame.quit()
 sys.exit()
--- game/maps_container.py
+++ game/maps_container.py
@@ -1,17 +1,33 @@
 import glob
 import os
-from utils import resourcepath
+import json
 
 
 def get():
-    target = os.path.join("maps/**", "*json")
-    clist = glob.glob(target)
+    target = os.path.join("./maps/**", "*json")
+    tmpd = glob.glob(target)
+    clist = []
+    for i in range(0, len(tmpd)):
+        clist.append([tmpd[i]])
+
+    for i in range(0, len(clist)):
+        with open(clist[i][0], 'r', encoding='UTF-8') as f:
+            data = json.load(f)
+            clist[i].append(json.dumps(data['title'], ensure_ascii=False))
+            clist[i].append(json.dumps(data['writer'], ensure_ascii=False))
+            clist[i].append(json.dumps(data['creator'], ensure_ascii=False))
+    print(clist)
     return clist
 
 
-def draw(screen, pg, screenx, screeny, maplist):
+def draw(screen, pg, screenx, screeny, maplist, BoldFont, LightFont):
     for mapnum in range(0, len(maplist)):
-        upbar = pg.Surface((screenx / 2.3, screeny / 6))
-        upbar.set_alpha(120 * (1/(mapnum+1)**0.4))
-        upbar.fill((0, 0, 0))
-        screen.blit(upbar, (1 + screenx - screenx / 2.3, screeny / 8 + (screeny/98 + screeny / 6)*mapnum))
+        container = pg.Surface((screenx / 2.3, screeny / 6))
+        container.set_alpha(120 * (1 / (mapnum + 1) ** 0.5))
+        container.fill((0, 0, 0))
+        screen.blit(container, (1 + screenx - screenx / 2.3, screeny / 8 + (screeny / 98 + screeny / 6) * mapnum))
+        title = BoldFont.render(maplist[mapnum][1], True, (255, 255, 255))
+        screen.blit(title, (10 + screenx - screenx / 2.3, screeny / 8 + (screeny / 98 + screeny / 6) * mapnum))
+        writer = LightFont.render("by " + maplist[mapnum][2], True, (255, 255, 255))
+        screen.blit(writer, (title.get_width() + 20 + screenx - screenx / 2.3,
+                             title.get_height() / 4 + screeny / 8 + (screeny / 98 + screeny / 6) * mapnum))
